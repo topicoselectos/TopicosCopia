bd15cda48cdf8554ca3e24e62ece17d84b3f20cc
edwardarce2000 <topsel2020@gmail.com>
2020-10-29 00:16:00 +0000

    Se crea el ASP de Orders

---
+++ Topicos/WebApplication1/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/order")]
+    public class OrdersController
+    {
+        [HttpGet]
+        public JsonResult GetOrders()
+        {
+            JsonResult elResultado = new JsonResult(new List<Object>() {
+                 new {id = 5, orderDate=DateTime. Now},
+                 new {id = 25, orderDate=DateTime. Now. AddMinutes(-200)}
+            });
+            return elResultado;
+        }
+    }
+}
