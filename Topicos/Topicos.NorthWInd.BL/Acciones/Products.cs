e391e65e70a8f80190cae9ae8b5506dd6b8133d8
edwardarce2000 <topsel2020@gmail.com>
2020-12-05 22:25:58 +0000

    Actualización

--- Topicos/Topicos.NorthWInd.BL/Acciones/Products.cs
+++ Topicos/Topicos.NorthWInd.BL/Acciones/Products.cs
@@ -6,12 +6,28 @@ namespace Topicos.NorthWInd.BL.Acciones
 {
     internal class Products
     {
+        private NorthWind.BaseDatos.Models.NorthWindContext context;
+
+        public Products(NorthWind.BaseDatos.Models.NorthWindContext context)
+        {
+            this.context = context;
+        }
+
         public NorthWind.BaseDatos.Models.Products ObtenerProductosPorId(int id)
         {
             NorthWind.BaseDatos.Models.Products elResultado;
             var elRepositorio = new Repositorio.Products();
             elResultado = elRepositorio.ObtenerProductosPorId(id);
             return elResultado;
+        }
+
+        internal NorthWind.BaseDatos.Models.Products ObtenerProductoPorId(int id)
+        {
+            NorthWind.BaseDatos.Models.Products elResultado;
+            var elRepositorio = new Repositorio.Products();
+            elResultado = elRepositorio.ObtenerProductosPorId(id);
+            return elResultado;
+
         }
     }
 }
